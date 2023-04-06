1fd5f1ffab23e954e6bc1ca4d3565b727d6b7307
Nate Brune <natebrune@gmail.com>
2023-04-05 17:27:01 -0700

    added cumulative vol, still some bugs

--- swap-arkive/entities/swap.ts
+++ swap-arkive/entities/swap.ts
@@ -3,19 +3,23 @@ import { createEntity } from "../deps.ts";
 interface ISwap {
   pair: String;
   amountIn: Number;
+  amountOut: Number;
   tradeDirection: Boolean;
   to: String;
   timestamp: Number;
   price0: Number,
-  price1: Number
+  price1: Number,
+  cumulativeVolume: Number
 }
 
 export const Swap = createEntity<ISwap>("Swap", {
   pair: String,
   amountIn: Number,
+  amountOut: Number,
   tradeDirection: Boolean,
   to: String,
   timestamp: Number,
   price0: Number,
-  price1: Number
+  price1: Number,
+  cumulativeVolume: Number,
 });
--- swap-arkive/handlers/swap.ts
+++ swap-arkive/handlers/swap.ts
@@ -7,22 +7,44 @@ export const swapHandler: EventHandlerFor<typeof uniswapV2Pair, "Swap"> =
     { event, client, store },
   ) => {
     const { amount0In, amount1In, amount0Out, amount1Out, to } = event.args;
-    console.log(amount0In, amount1In, amount0Out, amount1Out, to)
 
+    // Figure out price0 and price1
     const tradeDirection: boolean = (amount0Out < amount1Out); // False(0) if direction is token0, True(1) if direction is token1
-    console.log(tradeDirection)
+    let price: number = 0
+    if(tradeDirection){
+      price = Number((amount0In*BigInt(1e18))/amount1Out)/1e18
+    } else {
+      price = 1/((Number((amount1In*BigInt(1e18))/amount0Out))/1e18)
+    }
+    let cumulativeVolume: number  = await store.retrieve(
+      `${event.address}:lastSwapCumulativeVol`,
+      async () => {
+        return 0  
+      }
+    )
+    //
+    cumulativeVolume = cumulativeVolume + (tradeDirection ? Number(amount0In) : Number(amount0Out))
+    console.log(cumulativeVolume)
+    
+    //console.log(cumulativeVolume)
     // Create a new swap entry with the necessary information
     const newSwap = new Swap({
       pair: event.address,
-      amountIn: tradeDirection ? parseFloat(formatUnits(amount1In, 18)) : parseFloat(formatUnits(amount0In, 18)),
+      amountIn: tradeDirection ? parseFloat(formatUnits(amount0In, 18)) : parseFloat(formatUnits(amount1In, 18)),
+      amountOut: tradeDirection ? parseFloat(formatUnits(amount0Out,18)) : parseFloat(formatUnits(amount1Out, 18)),
       tradeDirection: tradeDirection,
-      amountOut: tradeDirection ? parseFloat(formatUnits(amount1Out,18)) : parseFloat(formatUnits(amount0Out, 18)),
       to: to,
       timestamp: parseFloat(formatUnits(event.blockNumber, 0)),
-      price0: tradeDirection ? parseFloat(formatUnits(amount1Out / (amount0In), 18)) : parseFloat(formatUnits(BigInt(1)/(amount0Out) / (amount1In), 18)),
-      price1: 0,
+      price0: price,
+      price1: 1/price,
+      //cumulativeVolume: formatUnits(BigInt(cumulativeVolume), 18)
   });
 
+
     // Save the new swap entry to the database
+    //store.set(`${event.address}:lastSwapCumulativeVol`, formatUnits(BigInt(cumulativeVolume), 18))
+    console.log(formatUnits(BigInt(cumulativeVolume), 18))
+    //console.log(formatUnits(BigInt(cumulativeVolume), 18))
     await newSwap.save();
+    
   };
