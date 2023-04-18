1c9d2bf972d722177e9df349403d3f4d4171f8fe
Nate Brune <natebrune@gmail.com>
2023-04-17 18:34:20 -0700

    add univ3 support, missing prices

---
+++ swap-arkive/entities/swapv3.ts
@@ -0,0 +1,135 @@
+import { createEntity } from "../deps.ts";
+
+interface ISwap {
+  pair: String;
+  hash: String;
+  to: String;
+  toName: String;
+  from: String;
+  fromName: String;
+  token0Address: String;
+  token1Address: String;
+  token0Symbol: String;
+  token1Symbol: String;
+  token0Name: String;
+  token1Name: String;
+  amountIn: Number;
+  amountOut: Number;
+  amountUSD: Number;
+  tradeDirection: Boolean;
+  router: String;
+  routerName: String;
+  timestamp: Number;
+  block: Number;
+  price0: Number,
+  price1: Number,
+  priceUSD: Number,
+  WETHUSD: Number,
+  cumulativeVolume0: Number,
+  cumulativeVolume1: Number,
+  cumulativeVolumeUSD: Number,
+  cumulativeFeesUSD: Number,
+  // reserves0: Number,
+  // reserves1: Number,
+  USDTVL: Number,
+  liquidity: Number,
+  tick: Number,
+  fee: Number
+}
+
+export const Swap = createEntity<ISwap>("Swap", {
+  pair: String,
+  hash: String,
+  to: {
+    type: String,
+    index: true,
+  },
+  toName: {
+    type: String,
+    index: true,
+  },
+  from: {
+    type: String,
+    index: true,
+  },
+  fromName: {
+    type: String,
+    index: true,
+  },
+  token0Address: {
+    type: String,
+    index: true,
+  },
+  token1Address: {
+    type: String,
+    index: true,
+  },
+  token0Symbol: {
+    type: String,
+    index: true,
+  },
+  token1Symbol: {
+    type: String,
+    index: true,
+  },
+  token0Name: {
+    type: String,
+    index: true,
+  },
+  token1Name: {
+    type: String,
+    index: true,
+  },
+  amountIn: Number,
+  amountOut: Number,
+  amountUSD: {
+    type: Number,
+    index: true,
+  },
+  tradeDirection: {
+    type: Boolean,
+    index: true,
+  },
+  router: {
+    type: String,
+    index: true,
+  },
+  routerName: {
+    type: String,
+    index: true,
+  },
+  timestamp: {
+    type: Number,
+    index: true,
+  },
+  block: {
+    type: Number,
+    index: true,
+  },
+  price0: Number,
+  price1: Number,
+  priceUSD: {
+    type: Number,
+    index: true,
+  },
+  WETHUSD: {
+    type: Number,
+    index: true,
+  },
+  cumulativeVolume0: Number,
+  cumulativeVolume1: Number,
+  cumulativeVolumeUSD: {
+    type: Number,
+    index: true,
+  },
+  cumulativeFeesUSD: {
+    type: Number,
+    index: true,
+  },
+  // reserves0: Number,
+  // reserves1: Number,
+  USDTVL: Number,
+  liquidity: Number,
+  tick: Number,
+  fee: Number
+});
--- swap-arkive/manifest.ts
+++ swap-arkive/manifest.ts
@@ -1,16 +1,16 @@
 import { Manifest } from "./deps.ts";
-import uniswapV2Pair from "./abis/uniswapV2Pair.ts";
+import uniswapV3Pair from "./abis/uniswapV3Pair.ts";
 import { Swap } from "./entities/swap.ts";
 import { Dex } from "./entities/dex.ts";
-import { swapHandler } from "./handlers/swap.ts";
+import { swapHandler } from "./handlers/swapv3.ts";
 
-const manifest = new Manifest("swap-arkive-v1");
+const manifest = new Manifest("swap-arkive-v2");
 
 manifest
 	.addEntity(Swap)
 	.addEntity(Dex)
 	.addChain("mainnet", { blockRange: 5n })
-	.addContract(uniswapV2Pair)
+	.addContract(uniswapV3Pair)
 	.addSource("*", 16962025n)
 	.addEventHandler("Swap", swapHandler);
 
