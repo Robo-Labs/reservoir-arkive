d2fdf72bd310ba58c0aa6f6ec2055b80374db4a9
Nathanial Brune <natebrune@protonmail.com>
2023-07-25 06:28:09 -0700

    bugfix

--- arkive/manifest.ts
+++ arkive/manifest.ts
@@ -10,7 +10,7 @@ import { AaveSnapshot } from './entities/aaveSnapshot.ts'
 import { SnapshotHandler } from './handlers/pairSnapshot.ts'
 import { PairSnapshot } from './entities/pairSnapshot.ts'
 
-const manifest = new Manifest('reservoir-mainnet')
+const manifest = new Manifest('reservoir-mainnet-birgus')
 const avalanche = manifest
 	.addEntities([Swap, Pair, Token, PairSnapshot, AaveSnapshot])
 	.addChain('avalanche', { blockRange: 1000n })
@@ -20,6 +20,8 @@ avalanche
 	.addSources({
 		'0x1e93509A80E936BfF8e27C129a9B99728A51D0cC': 31616108n,
 		'0x146D00567Cef404c1c0aAF1dfD2abEa9F260B8C7': 31569814n,
+		'0x02EE8b06c500d0DddB8c689efDF24971CeB0EF19': 33020397n,
+		'0x81E50D088987e8F96a7C5F909C5665cd267eec82': 33021482n
 	})
 	.addEventHandlers({ 'Swap': SwapHandler, 'Sync': SyncHandler })
 
